40d910d4d160bc3d1a5847c98f100577e91d5e0d
alexpataman[mac] <alex.pataman@gmail.com>
2022-02-07 15:16:53 +0100

    feat: improve navigation styles

--- src/views/Textbook/TextbookNav/TextbookNav.tsx
+++ src/views/Textbook/TextbookNav/TextbookNav.tsx
@@ -1,21 +1,45 @@
-import { Link } from 'react-router-dom';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+import NewReleasesIcon from '@mui/icons-material/NewReleases';
+import SchoolIcon from '@mui/icons-material/School';
+import { Grid } from '@mui/material';
+import Button from '@mui/material/Button';
+import { NavLink } from 'react-router-dom';
 
 import { AuthorisedOnly } from '../../../components/AuthorisedOnly/AuthorisedOnly';
 
+const links = [
+  {
+    to: '/textbook',
+    text: 'Все категории',
+    icon: <MenuBookIcon />,
+  },
+  {
+    to: '/textbook/difficult',
+    text: 'Сложные слова',
+    icon: <NewReleasesIcon />,
+  },
+  {
+    to: '/textbook/known',
+    text: 'Изученные слова',
+    icon: <SchoolIcon />,
+  },
+];
+
 export const TextbookNav = () => (
   <AuthorisedOnly>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/textbook">Все категории</Link>
-        </li>
-        <li>
-          <Link to="/textbook/difficult">Сложные</Link>
-        </li>
-        <li>
-          <Link to="/textbook/known">Изученные</Link>
-        </li>
-      </ul>
-    </nav>
+    <Grid container spacing={1}>
+      {links.map((link, index) => (
+        <Grid item key={index}>
+          <Button
+            component={NavLink}
+            to={link.to}
+            variant="outlined"
+            startIcon={link.icon}
+          >
+            {link.text}
+          </Button>
+        </Grid>
+      ))}
+    </Grid>
   </AuthorisedOnly>
 );
